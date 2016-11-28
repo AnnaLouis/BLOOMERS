10ebf96c163d6dfae23e39c163ec7352228483a6
Alizee Wyart <alizee.wyart@gmail.com>
2016-11-28 19:17:25 +0100

    new show bloomer

---
+++ app/assets/javascripts/bloomer_show.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".sub-title").on("click", function(e){
+      // Change active tab
+      $(".sub-title").removeClass("active");
+      $(this).addClass("active");
+      // Hide all tab-content (use class="hidden")
+      $(".sub-content").addClass("hidden");
+      // Show target tab-content (use class="hidden")
+      tabSelector = $(this).data("target");
+      $(tabSelector).removeClass("hidden");
+    });
+});
+
