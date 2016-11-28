d272ccd92a1f1f9c59fe9dc9bbcbeafc7dbc44cd
Felix Lienau <felix.lienau@gmail.com>
2016-11-28 12:05:34 +0100

    did the validations and co for startups

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,10 @@
 class Startup < ApplicationRecord
   belongs_to :user
+  has_many :batchs
+  has_many :candidatures, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :programs, through: :batchs
+  validates :short_description
+  validates :name
+  validates :date_creation
 end
