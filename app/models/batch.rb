9b5bcbc7338d2eb05612fe829e79ffcffe775314
Alizée WYART <alizee.wyart@gmail.com>
2016-11-28 12:25:56 +0100

    Update batch.rb

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,8 +2,7 @@ class Batch < ApplicationRecord
   belongs_to :startups
   belongs_to :programs
   belongs_to :bloomer, through: :programs
-  validates :name, dependent: :destroy
-  validates :batch_start, dependent: :destroy
-  validates :batch_end, dependent: :destroy
-
+  validates :name, presence: true, uniqueness: true
+  validates :batch_start, presence: true
+  validates :batch_end, presence: true
 end
