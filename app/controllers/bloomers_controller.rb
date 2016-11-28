5a886873c1c9736e0764850dd086bedef229ab61
Anna Louis <annalouis2@gmail.com>
2016-11-28 17:16:39 +0100

    controller bloomer

--- app/controllers/bloomers_controller.rb
+++ app/controllers/bloomers_controller.rb
@@ -1,4 +1,5 @@
 class BloomersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @bloomer = Bloomer.all
@@ -6,29 +7,37 @@ class BloomersController < ApplicationController
 
   def show
     @bloomer = Bloomer.find(params[:id])
-
   end
 
   def new
     @bloomer = Bloomer.new
+    @bloomer.user = current_user
+    authorize @bloomer
   end
 
   def create
     @bloomer = Bloomer.new(bloomer_params)
-    # @bloomer.user == user
-    @bloomer.save
+    @bloomer.user = current_user
+    authorize @bloomer
+    if @bloomer.save
+      redirect_to bloomer_path(@bloomer)
+    else
+      render :new
+    end
   end
 
   # def edit
+  #   authorize @bloomer
   # end
 
   # def update
+    # authorize @bloomer
   # end
 
 private
 
   def bloomer_params
-    params.require(:bloomer).permit(:name, :city, :country, :category, :address, :specialty, :date_creation)
+    params.require(:bloomer).permit(:name, :city, :country, :category, :website, :email, :twitter_account, :description, :address, :specialty, :date_creation)
   end
 
 end
