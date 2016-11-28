19706f38fc674ff4678a3314bd97db7cce38cd13
Brieuc Lambert <lambert.brieuc@gmail.com>
2016-11-28 18:59:39 +0100

    controller and forst view added for startup

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,24 +1,27 @@
 class StartupsController < ApplicationController
   def show
-    @startup = Startup.find([:id])
+    @startup = Startup.find(params[:id])
     authorize(@startup)
   end
 
   def new
     @startup = Startup.new
+    authorize(@startup)
   end
 
   def create
-    @startup = Startup.new(params[:id])
-  end
-
-  def edit
-
+    @startup = current_user.startups.new(startup_params)
+    authorize(@startup)
+    if @startup.save
+      redirect_to startup_path(@startup)
+    else
+      render 'new'
+    end
   end
 
 private
 
   def startup_params
-    params.require(startup).require(:create, :edit)
+    params.require(:startup).permit(:name, :description, :short_description, :date_creation, :twitter_account, :website)
   end
 end
