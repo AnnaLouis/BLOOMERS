5ec12fb692054cbbf2b0d7a5b6045cc811e73560
Alizee Wyart <alizee.wyart@gmail.com>
2016-11-28 16:15:18 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# LES TROIS STYLES D'USERS
+
+user1 = User.new(first_name: "Alizée", last_name:"Wyart")
+user1.email = "alizee@bloomers.fr"
+user1.password = "123456"
+user1.admin = false
+user1.startup_admin = true
+user1.bloomer_admin = false
+user1.save
+
+user2 = User.new(first_name: "Anna", last_name:"Louis")
+user2.email = "anna@bloomers.fr"
+user2.password = "123456"
+user2.startup_admin = false
+user2.bloomer_admin = true
+user2.admin = false
+user2.save
+
+user3 = User.new(first_name: "Bloom", last_name: "Her")
+user3.email = "hello@bloomers.fr"
+user3.password = "123456"
+user3.startup_admin = true
+user3.bloomer_admin = true
+user3.admin = true
+user3.save
+
+# STARTUP CREATION
+
+startup1 = Startup.new(name: "Ask Anna")
+startup1.short_description = "L’app pour ne plus hésiter en cabine d’essayage"
+startup1.website = "askanna.me"
+startup1.twitter_account = "https://twitter.com/AskAnna_app?utm_source=nouveau%20site&utm_medium=icons&utm_campaign=icones%20RS%20twitter%20new%20site"
+startup1.date_creation = Date.new(2015, 1, 1)
+startup1.user = User.find_by_email("alizee@bloomers.fr")
+startup1.save
+
+# BLOOMER CREATION
+
+bloomer1 = Bloomer.new(name:"Look Forward by Showroomprive")
+bloomer1.city = "La Plaine Saint Denis"
+bloomer1.country = "France"
+bloomer1.category = "Incubateur"
+bloomer1.website = "lookforwardproject.com"
+bloomer1.speciality = "mode and retail"
+bloomer1.email = "alizee.wyart@gmail.com"
+bloomer1.address = "1 rue des blés, ZA La Montjoie - 93 210 La Plaine Saint Denis"
+bloomer1.twitter_account = "https://twitter.com/LookForwardHub"
+bloomer1.date_creation = Date.new(2015, 6, 1)
+bloomer1.description = "LOOK FORWARD est la plateforme de toute innovation susceptible de revolutionner le monde de la mode et du retail. Avec les startups integrees à son incubateur, LOOK FORWARD a l’ambition de bousculer la facon de distribuer, consommer et produire la mode."
+bloomer1.user = User.find_by_email("anna@bloomers.fr")
+bloomer1.save
