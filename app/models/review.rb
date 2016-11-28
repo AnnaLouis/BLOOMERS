6ca1dc1409184c7fef700f12f5afa9fe1baf5127
Alizee Wyart <alizee.wyart@gmail.com>
2016-11-28 12:17:06 +0100

    init review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
+  # REFERENCES
   belongs_to :bloomer
   belongs_to :startup
+  has_many :users, through: :startups
+  # VALIDATIONS
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: (0..3).to_a,
+    message: "%{value} is not a correct rate" }
+  validates :rating, numericality: { only_integer: true }
 end
