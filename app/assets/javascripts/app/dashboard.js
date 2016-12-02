5e5fad7f371e84e6e535d72f85e44e3d4ae55ac1
Brieuc Lambert <lambert.brieuc@gmail.com>
2016-12-02 14:58:14 +0100

    tried to pee above my head but ended up taking a golden shower

---
+++ app/assets/javascripts/app/dashboard.js
@@ -0,0 +1,30 @@
+// $(function(){
+//   $(".dashboard-tab:first-child").addClass("active")
+//   $(".dashboard-tab").on("click", function(e){
+//     // Change active tab
+//     $(".dashboard-tab").removeClass("active");
+//     $(this).addClass("active");
+//     // Hide all tab-content (use class="hidden")
+//     $(".dashboard-tab-content").addClass("hidden");
+//     // Show target tab-content (use class="hidden")
+//     tabSelector = $(this).data("target");
+//     $(tabSelector).removeClass("hidden");
+//   });
+// });
+
+
+$(function(){
+  $(".dashboard-tab:first-child").each(function(index) {
+    $(this).addClass("active");
+  });
+   $(".dashboard-tab").on("click", function(e){
+      if $(this).hasClass('active') {
+
+      } else {
+        $(".dashboard-tab.active").removeClass("active");
+        $(this).addClass('active');
+        $(this)
+      }
+   });
+
+});
