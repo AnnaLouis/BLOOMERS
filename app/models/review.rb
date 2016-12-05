51f62421a16a1b258f5052f43bc906698972b18d
Felix Lienau <felix.lienau@gmail.com>
2016-12-05 16:33:04 +0100

    review problem solved

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :rating, presence: true
-  validates :rating, inclusion: { in: (0..3).to_a,
+  validates :rating, inclusion: { in: (0..5).to_a,
     message: "%{value} is not a correct rate" }
   validates :rating, numericality: { only_integer: true }
 
