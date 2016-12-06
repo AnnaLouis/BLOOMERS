88ed42c54a48741b7db82cd42517b16d30a4b4d4
Alizee Wyart <alizee.wyart@gmail.com>
2016-12-06 16:56:28 +0100

    new form

--- app/models/candidature.rb
+++ app/models/candidature.rb
@@ -3,4 +3,6 @@ class Candidature < ApplicationRecord
   belongs_to :program
   LIST_STATUS = ["new", "to meet", "rdv", "short list", "accepted", "refused"]
   validates :status, inclusion: { in: LIST_STATUS, message: "%{value} n'est pas une catégorie valide." }
+  validates :program_id, presence: true
+  validates :startup_id, presence: true
 end
